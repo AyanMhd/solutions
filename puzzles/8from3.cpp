c63947f43bf72814b02e72c66a70402db26a63df
maniac <mohammadayyan204@gmail.com>
2026-09-11 01:36:06 +0530

    Solved some puzzles

---
+++ puzzles/8from3.cpp
@@ -0,0 +1,4 @@
+A birthday cake arrives at a hotel party with eight guests eager to eat, but there’s time for only three quick straight cuts.  
+Can the cake be divided into eight equal pieces with just three cuts?
+
+Sol : Its a cake, not a circle.
